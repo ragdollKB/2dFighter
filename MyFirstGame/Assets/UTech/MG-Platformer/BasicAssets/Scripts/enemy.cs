97bd28f480f7f8fd457cd318c86d6ca6b9a51a98
RagdollKB <kanton@ohthehum.com>
2019-09-23 18:55:57 -0700

    Upload of 2d Game

---
+++ MyFirstGame/Assets/UTech/MG-Platformer/BasicAssets/Scripts/enemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    public Transform Player;
+     // Use this for initialization
+     void Start () {
+         
+     }
+     
+     // Update is called once per frame
+     void Update ()
+         
+     {
+ 
+         GetComponent<UnityEngine.AI.NavMeshAgent>().destination = Player.position;
+     }
+}
+ 
